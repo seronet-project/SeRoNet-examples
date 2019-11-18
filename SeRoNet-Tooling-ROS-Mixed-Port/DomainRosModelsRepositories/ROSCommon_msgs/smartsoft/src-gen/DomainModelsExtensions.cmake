afc5d8c1c4b3867abff30f02056b0d40ff5abda5
ipa-nhg <nhg@ipa.fhg.de>
2019-11-18 17:36:06 +0100

    WIP - fix autogenerated code to at least be compilable

---
+++ SeRoNet-Tooling-ROS-Mixed-Port/DomainRosModelsRepositories/ROSCommon_msgs/smartsoft/src-gen/DomainModelsExtensions.cmake
@@ -0,0 +1,13 @@
+# coordination interfaces extensions
+GET_FILENAME_COMPONENT(Coordination_DIR "${PROJECT_SOURCE_DIR}/../coordination" REALPATH)
+IF(EXISTS ${Coordination_DIR})
+MESSAGE("-- Include CoordinationInterfaces")
+INCLUDE("${Coordination_DIR}/src-gen/coordinationInterfaces.cmake")
+ENDIF(EXISTS ${Coordination_DIR})
+
+# ROSCommon_msgsOpcUaBackend
+GET_FILENAME_COMPONENT(OPC_UA_BACKEND_DIR "${PROJECT_SOURCE_DIR}/../opcua-backend" REALPATH)
+IF(EXISTS ${OPC_UA_BACKEND_DIR})
+	ADD_SUBDIRECTORY(${OPC_UA_BACKEND_DIR}/src-gen ${CMAKE_CURRENT_BINARY_DIR}/opcua-backend)
+ENDIF(EXISTS ${OPC_UA_BACKEND_DIR})
+
